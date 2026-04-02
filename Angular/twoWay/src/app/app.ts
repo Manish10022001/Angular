51163b581823e634ec7e76909709b10e1e00bd55
Manish10022001 <manishshirsat2850@gmail.com>
2026-04-02 21:32:09 +0530

    Task 3: two way data binding

--- Angular/twoWay/src/app/app.ts
+++ Angular/twoWay/src/app/app.ts
@@ -7,10 +7,16 @@ import { Component, signal } from '@angular/core';
   styleUrl: './app.css',
 })
 export class App {
-  name: any;
+  nam: any;
   x: any;
   fun() {
     let el = document.getElementById('name') as HTMLInputElement; //here used type assertion, as in angular we cannot directly get value, so we use type assertion
-    this.name = el.value;
+    this.nam = el.value;
+  }
+  name!: string; //if there is not initialer then we can either set it to entry string  or give ! before colon
+  email!: string;
+  password!: string;
+  submit() {
+    alert(`Successfully Login \n ${this.name} \n ${this.email} \n${this.password}`);
   }
 }
