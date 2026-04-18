9498e9b3a72933038ca11b2fed55ad9578042a69
Manish10022001 <manishshirsat2850@gmail.com>
2026-04-18 16:31:30 +0530

    Component Communication: added comments for @ViewChild

--- Angular/outputCounter/src/app/app.ts
+++ Angular/outputCounter/src/app/app.ts
@@ -13,6 +13,10 @@ export class App {
   receive(str: string) {
     this.receivedData = str;
   }
+
+  // if someone clicks on +,- or reset method, count value increment or decrement or resut based on button cliced in header component,
+  // now main parent component see this header componnent and names it hc, now moment we click on click button from app.html it calls method in app component
+  //in htis methed we are taking this value from component, and give that value to c, and printing it on screen
   c = 0;
   @ViewChild(Header) hc!: Header;
   received() {
