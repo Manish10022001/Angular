ddfaeb2165c86ad6f6da51f85cb5d4a3405a7e0f
Manish10022001 <manishshirsat2850@gmail.com>
2026-04-18 23:48:52 +0530

    Task 4: Component Communication: @ViewChild

---
+++ Tasks/TaskFour/src/app/app-module.ts
@@ -0,0 +1,14 @@
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { Header } from './header/header';
+
+@NgModule({
+  declarations: [App, Header],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  providers: [provideBrowserGlobalErrorListeners()],
+  bootstrap: [App],
+})
+export class AppModule {}
